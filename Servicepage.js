76aed87926e73dccdd6e1ae4a8c5b5883b64d436
Vasanthan <srvasanthan33@outlook.com>
2023-04-25 11:33:17 +0530

    Route and param are added for middleware fucntionality

--- Servicepage.js
+++ Servicepage.js
@@ -24,23 +24,31 @@ const Services = [
     },
     {
         id: 6,
-        name: "Cloud architectr"
+        name: "Cloud architect"
     }
 ];
 
-router.get("/:no", (request, response) => {
-    // if (request.params.service == 1) {
-    //     response.send("Service from App Development " + typeof request.params.service);
-    // } else if (request.params.service == 2) {
-    //     response.send("Sevice from Web development");
-    // } else {
-    //     response.send("From IT Support");
-    // }
-    const Service_id = Number(request.params.no);
-    const findService = Services.find(serve => serve.id === Service_id);
-
-    if (findService) response.json(findService);
-    else response.status(500).send("Invalid Input");
+router.route("/:id").get((request, response) => {
+    response.send(`Role : ${request.services.name}`);
 });
 
+router.param("id", (request, response, next, id) => {
+    console.log(id);
+    request.services = Services[id];
+    console.log(request.services);
+    next();
+});
 module.exports = router;
+
+// if (request.params.service == 1) {
+//     response.send("Service from App Development " + typeof request.params.service);
+// } else if (request.params.service == 2) {
+//     response.send("Sevice from Web development");
+// } else {
+//     response.send("From IT Support");
+// }
+// const Service_id = Number(request.params.no);
+// const findService = Services.find(serve => serve.id === Service_id);
+
+// if (findService) response.json(findService);
+// else response.status(500).send("Invalid Input");
